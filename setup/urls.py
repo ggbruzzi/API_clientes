bdbfc064170d82e29d344b3ae879f4e63062cf6a
ggbruzzi <guilhermebruzzi20@gmail.com>
2023-01-03 13:36:26 -0300

    adicionando ordenação e filtros

--- setup/urls.py
+++ setup/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import routers
 from clientes.views import ClientesViewSet
 
+
 router = routers.DefaultRouter()
 router.register('clientes', ClientesViewSet)
 
